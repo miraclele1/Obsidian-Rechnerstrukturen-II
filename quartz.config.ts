91db5d8042a691a1903753924ba3659c4b354ce4
Miras Kassym <miras.kassym@hs-augsburg.de>
2026-04-09 23:01:30 +0200

    Quartz sync: Apr 9, 2026, 11:01 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "miraclele1.github.io/Obsidian-Rechnerstrukturen-II",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
